87167dd81bafc2c2508d1adddace78f16066a932
Jonas De Jesus Da Silva <jonas.jsilva@e-ai.com.br>
2022-09-20 20:31:02 -0300

    [jonas_si_ev] Feito novas implementações no front-end

--- Front/SistemaEventos-App/src/app/eventos/eventos.component.ts
+++ Front/SistemaEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  larguraImagem: number = 150;
+  margemimagem: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {local: any; tema: string;}) =>
+      evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+
+    )
+  }
+
+
 
   constructor(private http: HttpClient) { }
 
@@ -16,14 +43,22 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  visualizarImagem(){
+    this.exibirImagem = !this.exibirImagem;
+  }
+
 
 public getEventos(): void{
 
   this.http.get('https://localhost:5001/api/eventos').subscribe(
-    response => this.eventos = response,
+    response => {
+      this.eventos = response,
+      this.eventosFiltrados = this.eventos
+
+    },
     error => console.log(error)
   );
-  
+
 }
 
 }
